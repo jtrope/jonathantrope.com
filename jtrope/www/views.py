f6d9d9120fca53c32d8398a7c0e22920cae373a4
Jonathan Trope <jonathan@jonathantrope.com>
2018-11-10 13:07:20 -0500

    Setup image endpoint

--- jtrope/www/views.py
+++ jtrope/www/views.py
@@ -1,4 +1,20 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the www index.")
+
+
+def images(request):
+    # TODO: Put in config
+    images = [
+        {
+            'caption': 'Yellowstone Bison',
+            'url': 'http://www.placecage.com/200/200',
+        },
+        {
+            'caption': 'Thailand Street Food',
+            'url': 'http://www.placecage.com/100/100',
+        }
+    ]
+    return JsonResponse(images, safe=False)
