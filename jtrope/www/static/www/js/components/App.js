2e93b4e6b679dff1548db22a69066bfbfe132ba9
Jonathan Trope <jonathan@jonathantrope.com>
2018-11-19 21:49:14 -0500

    render random image

--- jtrope/www/static/www/js/components/App.js
+++ jtrope/www/static/www/js/components/App.js
@@ -1,8 +1,21 @@
 import React from "react";
 
-const App = () => (
-    <div>HELLO WORLD FROM APP COMPONENT</div>
-);
+class App extends React.Component {
+  constructor(props) {
+      props.images = ['http://www.placecage.com/200/200', 'http://www.placecage.com/100/100']
+      super(props);
+      const currImg = this.props.images[Math.floor(Math.random()*this.props.images.length)];
+      this.state = {currImg: currImg};
+  }
+
+  render() {
+      return (
+          <div>
+              Hi, my name is Jonathan. I like to make cool stuff on the Internet
+              <img src={this.state.currImg} />
+          </div>
+      );
+  }
+}
 
 export default App;
-
