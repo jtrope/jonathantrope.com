8b216fd242f4ff8c5f68919fc2669e55bc546881
Jonathan Trope <jonathan@jonathantrope.com>
2018-11-20 15:23:14 -0500

    Updates webpack for dev mode, adds moar FE functionality

--- jtrope/www/static/www/js/components/App.js
+++ jtrope/www/static/www/js/components/App.js
@@ -1,21 +1,43 @@
 import React from "react";
 
 class App extends React.Component {
-  constructor(props) {
-      props.images = ['http://www.placecage.com/200/200', 'http://www.placecage.com/100/100']
-      super(props);
-      const currImg = this.props.images[Math.floor(Math.random()*this.props.images.length)];
-      this.state = {currImg: currImg};
-  }
+    constructor(props) {
+        // TODO: Replace hardcoded with API call
+        super(props);
+        const currImg = this.props.images[Math.floor(Math.random()*this.props.images.length)];
+        this.state = {currImg: currImg};
 
-  render() {
-      return (
-          <div>
-              Hi, my name is Jonathan. I like to make cool stuff on the Internet
-              <img src={this.state.currImg} />
-          </div>
-      );
-  }
+        // Bindings
+        this._handleClick =this._handleClick.bind(this);
+    }
+
+    static defaultProps = {
+        images: [
+            'http://www.placecage.com/100/100',
+            'http://www.placecage.com/200/200',
+            'http://www.placecage.com/300/300',
+            'http://www.placecage.com/400/400',
+        ]
+    };
+
+    render() {
+        return (
+            <div>
+                Hi, my name is Jonathan. I like to make cool stuff on the Internet
+                <a href="#" onClick={this._handleClick}>Get random image</a>
+                <img src={this.state.currImg} />
+            </div>
+        );
+    }
+
+    _handleClick(e) {
+        e.preventDefault();
+        this.setState({currImg: this._getRandomImg()})
+    }
+
+    _getRandomImg() {
+        return this.props.images[Math.floor(Math.random()*this.props.images.length)];
+    }
 }
 
 export default App;
