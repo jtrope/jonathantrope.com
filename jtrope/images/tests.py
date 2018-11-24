8cb8af0c76d5ff12028594bb343503ed60c6b9fc
Jonathan Trope <jonathan@jonathantrope.com>
2018-11-24 14:20:37 -0500

    Add a no nonsense test for images API

--- jtrope/images/tests.py
+++ jtrope/images/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class ImageIndexViewTests(TestCase):
+
+    def test_returns_images(self):
+        resp = self.client.get('/api/images/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue(resp.json())
